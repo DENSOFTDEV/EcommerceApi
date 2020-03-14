50ed5195e6354bd9106dc3ca51421f0543d60ad0
DENSOFTDEV <wakahiad@gmail.com>
2020-03-14 21:42:03 +0300

    Migrations, models , table relationships and db seeder made

--- app/User.php
+++ app/User.php
@@ -9,30 +9,48 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $table = 'users';
+
+    const VERIDIED_USER = '1';
+    const UNVERIDIED_USER = '2';
+
+    const ADMIN_USER = 'true';
+    const REGULAR_USER = 'false';
+
+
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'admin'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'verification_token'
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isVerified()
+    {
+        return $this->verified == User::VERIDIED_USER;
+    }
+
+    public function isAdmin()
+    {
+        return $this->admin == User::ADMIN_USER;
+    }
+
+    public static function generateVerificationCode()
+    {
+        return str_random(40);
+    }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,10 @@
 
 /** @var Factory $factory */
 
+use App\Category;
+use App\Product;
+use App\Seller;
+use App\Transaction;
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factory;
@@ -22,8 +26,43 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'verified' => $verified = $faker->randomElement([User::VERIDIED_USER, User::UNVERIDIED_USER]),
+        'verification_token' => $verified == User::VERIDIED_USER ? null : User:: generateVerificationCode(),
+        'admin' => $verified = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
+    ];
+});
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.png', '2.png', '3.png']),
+        'seller_id' => User::all()->random()->id,
+
+        //To obtain seller id you can also execute
+        //User::inRandomOrder()->first()->id,
+    ];
+});
+$factory->define(Transaction::class, function (Faker $faker) {
+
+
+    $seller = Seller::has('products')->get()->random();
+    $buyer = User::all()->except($seller->id)->random();
+
+    return [
+        'quantity' => $faker->numberBetween(1, 3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
     ];
 });
