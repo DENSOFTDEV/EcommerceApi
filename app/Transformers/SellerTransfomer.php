45c74b69e6599807d4ad3a1846f81ee1a1762161
DENSOFTDEV <wakahiad@gmail.com>
2020-03-26 20:20:12 +0300

    Implementing HATEOAS Hypermedia Controls

--- app/Transformers/SellerTransfomer.php
+++ app/Transformers/SellerTransfomer.php
@@ -40,6 +40,30 @@ class SellerTransfomer extends TransformerAbstract
             'creationDate' => (string)$seller->created_at,
             'lastChange' => (string)$seller->updated_at,
             'deletedDate' => isset($seller->deleted_at) ? (string)$seller->deleted_at : null,
+
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('sellers.show', $seller->id)
+                ],
+                [
+                    'rel' => 'seller.categories',
+                    'href' => route('sellers.categories.index', $seller->id)
+                ],
+                [
+                    'rel' => 'seller.products',
+                    'href' => route('sellers.products.index', $seller->id)
+                ],
+                [
+                    'rel' => 'seller.transactions',
+                    'href' => route('sellers.transactions.index', $seller->id)
+                ],
+                [
+                    'rel' => 'seller',
+                    'href' => route('users.show', $seller->id)
+
+                ]
+            ]
         ];
     }
 
